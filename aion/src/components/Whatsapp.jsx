c125d11a158b2626cc36852200c9c33caf41babc
Bakumpe <bakumpejoseph@gmail.com>
2025-03-08 08:23:28 +0300

    Watsap

--- aion/src/components/Whatsapp.jsx
+++ aion/src/components/Whatsapp.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import Watsap from "../assets/watsap.png";
 
 function Whatsapp() {
   return (
     <>
       <div className="whatsap">
-        <img src={Watsap} alt="Whatsapp" />
+        <NavLink to={'https://wa.me/256768520373'}>
+          <img src={Watsap} alt="Whatsapp" />
+        </NavLink>
       </div>
     </>
   );
