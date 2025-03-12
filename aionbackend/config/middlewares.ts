f139630116435cb7c48c56277de712085585f8be
Bakumpe <bakumpejoseph@gmail.com>
2025-03-12 03:05:11 +0300

    Aion Add  Proxy

--- aionbackend/config/middlewares.ts
+++ aionbackend/config/middlewares.ts
@@ -1,48 +1,3 @@
-// export default [
-//   'strapi::logger',
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
-
-// export default [
-//   'strapi::logger',
-//   'strapi::errors',
-//   'strapi::security',
-//   {
-//     name: 'strapi::cors',
-//     config: {
-//       origin: ['https://aiongetaion.netlify.app', 'http://localhost:5173'], // Allow all origins. You can specify allowed origins like ['http://example.com']
-//       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-//       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-//     },
-//   },
-//   'strapi::poweredBy',
-//   'strapi::query',
-//   {
-//     name: 'strapi::body',
-//     config: {
-//       formLimit: '4096mb', // Modify form body
-//       jsonLimit: '4096mb', // Modify JSON body
-//       textLimit: '4096mb', // Modify text body
-//       formidable: {
-//         maxFileSize: 1024 * 1024 * 1024, // Modify limit of uploaded file size to 1GB
-//       },
-//     },
-//   },
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-
-
 export default [
   'strapi::logger',
   'strapi::errors',
@@ -52,7 +7,7 @@ export default [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'], // Make sure to include all required endpoints
+          'connect-src': ["'self'", 'https:'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
@@ -63,8 +18,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://aiongetaion.netlify.app', 'http://localhost:5173'],
-      // origin: '*', // Allow specific origins
+      origin: ['https://aiongetaion.netlify.app', 'http://localhost:5173'], // Allow specific origins
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
@@ -86,6 +40,3 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
-
-
-
